eed6648b89b9d05b5fb1d38c528266dde064fd50
Jimcl <81977373+Jimcl6@users.noreply.github.com>
2025-03-07 16:25:59 +0800

    Update content-event.php

--- wp-content/themes/fictional-university/template-parts/content-event.php
+++ wp-content/themes/fictional-university/template-parts/content-event.php
@@ -13,7 +13,7 @@
     <div class="event-summary__content">
         <h5 class="event-summary__title headline headline--tiny"><a
                 href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-        <p><? echo (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?><a
+        <p><?= (has_excerpt()) ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?><a
                 href="<?php the_permalink(); ?>" class="nu gray">Learn
                 more</a>
         </p>
